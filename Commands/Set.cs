117bd7f1cd11b4d973c005024b78f577ae417747
alfaproject <alfa.project@gmail.com>
2009-04-02 14:30:26 +0100

    Improved !set goal. The goal is now optional and can be set before the skill name.

--- Commands/Set.cs
+++ Commands/Set.cs
@@ -53,19 +53,30 @@ namespace BigSister {
     }
 
     private static void SetGoal(CommandContext bc) {
-      if (bc.MessageTokens.Length < 4) {
-        bc.SendReply("Syntax: !set goal <skill> <goal>");
+      if (bc.MessageTokens.Length < 3) {
+        bc.SendReply("Syntax: !set goal <skill> [goal]");
         return;
       }
 
-      string skill = Skill.OVER;
-      if (!Skill.TryParse(bc.MessageTokens[2], ref skill)) {
+      string skill = null;
+      string goal = null;
+      if (Skill.TryParse(bc.MessageTokens[2], ref skill)) {
+        if (bc.MessageTokens.Length > 3) {
+          // !set goal <skill> <goal>
+          goal = bc.MessageTokens[3];
+        } // else { // !set goal <skill> }
+      } else if (bc.MessageTokens.Length > 3 && Skill.TryParse(bc.MessageTokens[3], ref skill)) {
+        // !set goal <goal> <skill>
+        goal = bc.MessageTokens[2];
+      } else {
         bc.SendReply("Error: Invalid skill name.");
         return;
       }
 
-      string goal = bc.MessageTokens[3].ToLowerInvariant();
-      if (!Regex.Match(goal, @"n(?:l|r)|r?\d+(?:\.\d+)?(?:m|k)?").Success) {
+      if (goal == null) {
+        // Get goal from database
+        goal = Database.GetStringParam("users", "goals", "fingerprint='" + bc.From.FingerPrint + "'", skill, "nl");
+      } else if (!Regex.Match(goal, @"n(?:l|r)|r?\d+(?:\.\d+)?(?:m|k)?").Success) {
         bc.SendReply("Error: Invalid goal.");
         return;
       }
@@ -76,31 +87,31 @@ namespace BigSister {
       }
 
       if (goal.EqualsI("nl")) {
-        bc.SendReply(@"Your goal for \b{0}\b is now set to \unext level\u.".FormatWith(skill));
+        bc.SendReply(@"Your goal for \b{0}\b is currently set to \unext level\u.".FormatWith(skill));
       } else if (goal.EqualsI("nr")) {
-        bc.SendReply(@"Your goal for \b{0}\b is now set to \unext rank\u.".FormatWith(skill));
+        bc.SendReply(@"Your goal for \b{0}\b is currently set to \unext rank\u.".FormatWith(skill));
       } else if (goal.StartsWithI("r")) {
         int goalRank = goal.Substring(1).ToInt32();
         if (goalRank > 0 && goalRank <= 2000000) {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \urank {1}\u.".FormatWith(skill, goalRank.ToShortString(1)));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \urank {1}\u.".FormatWith(skill, goalRank.ToShortString(1)));
           goal = "r" + goalRank.ToString();
         } else {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \unext rank\u.".FormatWith(skill));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \unext rank\u.".FormatWith(skill));
           goal = "nr";
         }
       } else {
         int goalLevel = goal.ToInt32();
         if (goalLevel > 1 && goalLevel < 127) {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \ulevel {1}\u.".FormatWith(skill, goalLevel));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \ulevel {1}\u.".FormatWith(skill, goalLevel));
           goal = goalLevel.ToString();
         } else if (goalLevel == 127) {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \u200m exp\u.".FormatWith(skill));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \u200m exp\u.".FormatWith(skill));
           goal = goalLevel.ToString();
         } else if (goalLevel > 127 && goalLevel <= 200000000) {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \u{1} exp\u.".FormatWith(skill, goalLevel.ToShortString(1)));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \u{1} exp\u.".FormatWith(skill, goalLevel.ToShortString(1)));
           goal = goalLevel.ToString();
         } else {
-          bc.SendReply(@"Your goal for \b{0}\b is now set to \unext level\u.".FormatWith(skill));
+          bc.SendReply(@"Your goal for \b{0}\b is currently set to \unext level\u.".FormatWith(skill));
           goal = "nl";
         }
       }
