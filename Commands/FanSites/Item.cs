8ecd87911728c2727169dfdee0ae718af42e29c3
alfaproject <alfa.project@gmail.com>
2009-04-22 12:19:30 +0100

    Code clean up.

--- Commands/FanSites/Item.cs
+++ Commands/FanSites/Item.cs
@@ -21,7 +21,7 @@ namespace BigSister {
 
         switch (items.Count) {
           case 0:
-            bc.SendReply("\\c12www.tip.it\\c doesn't have any record for item \"{0}\".".FormatWith(query));
+            bc.SendReply(@"\c12www.tip.it\c doesn't have any record for item ""{0}"".".FormatWith(query));
             return;
           case 1:
             item = items[0];
@@ -29,30 +29,31 @@ namespace BigSister {
             break;
           default:
             string reply = @"\c12www.tip.it\c has \c07{0}\c items:".FormatWith(items.Count);
-            for (int i = 0; i < Math.Min(14, items.Count); i++)
-              reply += " \\c07#{0}\\c {1};".FormatWith(items[i].Id, items[i].Name);
-            if (items.Count > 14)
+            for (int i = 0; i < Math.Min(14, items.Count); i++) {
+              reply += @" \c07#{0}\c {1};".FormatWith(items[i].Id, items[i].Name);
+            }
+            if (items.Count > 14) {
               reply += " ...";
+            }
             bc.SendReply(reply);
             return;
         }
       }
 
       if (item.Name == null) {
-        bc.SendReply("\\c12www.tip.it\\c doesn't have any record for item \\c07#{0}\\c.".FormatWith(itemId));
+        bc.SendReply(@"\c12www.tip.it\c doesn't have any record for item \c07#{0}\c.".FormatWith(itemId));
       } else {
-        bc.SendReply("\\c07{0}\\c | Alch: \\c07{1}/{2}\\c | MarketPrice: \\c07{3}\\c | Location: \\c07{4}\\c | \\c12www.tip.it/runescape/index.php?rs2item_id={5}\\c".FormatWith(
-                                   item.Name,
-                                   item.HighAlch.ToShortString(1), item.LowAlch.ToShortString(1),
-                                   item.MarketPrice.ToShortString(1),
-                                   item.Location,
-                                   item.Id));
-        bc.SendReply("Members? \\c{0}\\c | Quest: \\c{1}\\c | Tradeable? \\c{2}\\c | Stackable? \\c{3}\\c | Weight? \\c07{4}Kg\\c | Examine: \\c07{5}\\c".FormatWith(
-                                   item.Members ? "3Yes" : "4No",
-                                   item.Quest ? "3Yes" : "4No",
-                                   item.Tradable ? "3Yes" : "4No",
-                                   item.Stackable ? "3Yes" : "4No",
-                                   item.Weight, item.Examine));
+        bc.SendReply(@"\c07{0}\c | Alch: \c07{1}/{2}\c | MarketPrice: \c07{3}\c | Location: \c07{4}\c | \c12www.tip.it/runescape/index.php?rs2item_id={5}\c".FormatWith(
+                     item.Name,
+                     item.HighAlch.ToShortString(1), item.LowAlch.ToShortString(1),
+                     item.MarketPrice.ToShortString(1),
+                     item.Location, item.Id));
+        bc.SendReply(@"Members? \c{0}\c | Quest: \c{1}\c | Tradeable? \c{2}\c | Stackable? \c{3}\c | Weight? \c07{4}Kg\c | Examine: \c07{5}\c".FormatWith(
+                     item.Members ? "3Yes" : "4No",
+                     item.Quest ? "3Yes" : "4No",
+                     item.Tradable ? "3Yes" : "4No",
+                     item.Stackable ? "3Yes" : "4No",
+                     item.Weight, item.Examine));
       }
     }
 
--- FanSites/Items.cs
+++ FanSites/Items.cs
@@ -10,8 +10,9 @@ namespace BigSister {
       try {
         string itemsPage = new System.Net.WebClient().DownloadString("http://www.tip.it/runescape/index.php?rs2item=&orderby=0&keywords=" + query + "&Players=all&category=0&subcategory=0&cmd=8&action=Manage_Items&search=1&submit=Simple+Search");
 
-        foreach (Match itemMatch in Regex.Matches(itemsPage, @"<tr>\s+<td><a href=""index\.php\?rs2item_id=(\d+)"">([^<]+)</a></td>", RegexOptions.Singleline))
+        foreach (Match itemMatch in Regex.Matches(itemsPage, @"rs2item_id=(\d+)"">([^<]+)")) {
           this.Add(new Item(int.Parse(itemMatch.Groups[1].Value, CultureInfo.InvariantCulture), itemMatch.Groups[2].Value));
+        }
       } catch {
       }
     }
