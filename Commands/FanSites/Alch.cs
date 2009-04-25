439278d91db56e8fa25a1a5392475b687ddf4817
alfaproject <alfa.project@gmail.com>
2009-04-25 01:15:57 +0100

    Rewrote alch command.

--- Commands/FanSites/Alch.cs
+++ Commands/FanSites/Alch.cs
@@ -12,80 +12,65 @@ namespace BigSister {
       }
 
       int totalHigh = 0, totalLow = 0;
-      string totalItem = string.Empty;
-      bc.Message = bc.Message.Substring(bc.Message.IndexOf(' ') + 1);
-      foreach (string token in bc.Message.Split('+')) {
-        string[] itemLine = token.Trim().Split(' ');
+      string itemList = string.Empty;
 
-        double qty;
-        string inputItem;
-        if (itemLine.Length > 1 && MathParser.TryCalc(itemLine[0], out qty)) {
+      foreach (string messageToken in bc.Message.Substring(bc.Message.IndexOf(' ') + 1).Split('+')) {
+        string[] queryTokens = messageToken.Trim().Split(new char[] { ' ' }, 2);
+
+        double queryQty;
+        string queryItem;
+        if (queryTokens.Length == 2 && MathParser.TryCalc(queryTokens[0], out queryQty)) {
           // <qty> <item>
-          qty = Math.Max(1, Math.Floor(qty));
-          inputItem = itemLine.Join(1);
+          queryQty = Math.Max(1.0, Math.Floor(queryQty));
+          queryItem = queryTokens[1];
         } else {
           // <item>
-          qty = 1;
-          inputItem = itemLine.Join();
+          queryQty = 1.0;
+          queryItem = queryTokens.Join();
         }
 
-        int highAlch, lowAlch, itemID;
-        string itemName, itemList;
-        
-        if (int.TryParse(inputItem, out itemID)) {
-          Item item = new Item(itemID);
-          if (item.Name == null) {
-            bc.SendReply("\\c12www.tip.it\\c doesn't have any record for item \\c07#{0}\\c.".FormatWith(itemID));
-            return;
-          }
-          totalHigh += (int)qty * item.HighAlch;
-          totalLow += (int)qty * item.LowAlch;
-          totalItem += "\\c07" + qty + "\\c " + item.Name + " + ";
-        } else if (_GetAlch(inputItem, out itemName, out highAlch, out lowAlch, out itemList)) {
-            totalHigh += (int)qty * highAlch;
-            totalLow += (int)qty * lowAlch;
-            totalItem += "\\c07" + qty + "\\c " + itemName + " + ";
+        Item item;
+
+        int itemId;
+        if (int.TryParse(queryItem.TrimStart('#'), out itemId)) {
+          item = new Item(itemId);
         } else {
-          if (itemList != string.Empty) { bc.SendReply("\\c12www.tip.it\\c found more than one result for \"{0}\": {1}".FormatWith(inputItem, itemList)); return; }
-          bc.SendReply("\\c12www.tip.it\\c doesn't have any record for item \"{0}\". You can use !item to search for correct item names.".FormatWith(inputItem));
-          return;
-        }
-      }
-      bc.SendReply("{0} | HighAlch: \\c07{1:N0}\\c | LowAlch: \\c07{2:N0}\\c".FormatWith(
-                                 totalItem.Substring(0, totalItem.Length - 3), totalHigh, totalLow));
-    }
+          Items items = new Items(queryItem);
 
-    private static bool _GetAlch(string itemName, out string name, out int highAlch, out int lowAlch, out string itemList) {
-      name = null;
-      highAlch = 0;
-      lowAlch = 0;
-      itemList = string.Empty;
-      try {
-        string items_page = new System.Net.WebClient().DownloadString("http://www.tip.it/runescape/index.php?rs2item=&orderby=0&keywords={0}&Players=all&category=0&subcategory=0&cmd=8&action=Manage_Items&search=1&submit=Simple+Search".FormatWith(itemName));
-        Match M = Regex.Match(items_page, "<strong>(\\d+)<\\/strong> items matched your search.");
-        if (M.Groups[1].Value.ToInt32() > 0) {
-          // items search page
-          MatchCollection items = Regex.Matches(items_page, "item_id=(\\d+)\">([^<]+)<", RegexOptions.Singleline);
-          if (items.Count == 0) {
-            return false;
-          } else if (items.Count == 1) {
-            Item item = new Item(int.Parse(items[0].Groups[1].Value, CultureInfo.InvariantCulture));
-            name = item.Name;
-            highAlch = item.HighAlch;
-            lowAlch = item.LowAlch;
-            return true;
-          } else {
-            foreach (Match itemMatch in items) {
-              if (itemList.Length < 300) {
-                itemList += "\\c07#{0}\\c {1}; ".FormatWith(itemMatch.Groups[1].Value, itemMatch.Groups[2].Value);
+          switch (items.Count) {
+            case 0:
+              bc.SendReply(@"\c12www.tip.it\c doesn't have any record for item ""{0}"".".FormatWith(queryItem));
+              return;
+            case 1:
+              item = items[0];
+              item.LoadFromWeb();
+              break;
+            default:
+              string reply = @"\c12www.tip.it\c has \c07{0}\c items matching ""{1}"":".FormatWith(items.Count, queryItem);
+              for (int i = 0; i < Math.Min(14, items.Count); i++) {
+                reply += @" \c07#{0}\c {1};".FormatWith(items[i].Id, items[i].Name);
               }
-            }
+              if (items.Count > 14) {
+                reply += " ...";
+              }
+              bc.SendReply(reply);
+              return;
           }
         }
-        itemList = itemList.Substring(0, itemList.Length - 1);
-      } catch {
+
+        if (item.Name == null) {
+          bc.SendReply(@"\c12www.tip.it\c doesn't have any record for item \c07#{0}\c.".FormatWith(itemId));
+          return;
+        } else {
+          totalHigh += (int)queryQty * item.HighAlch;
+          totalLow += (int)queryQty * item.LowAlch;
+          itemList += @"\c07{0:N0}\c {1} + ".FormatWith(queryQty, item.Name);
+        }
+
       }
-      return false;
+
+      bc.SendReply(@"{0} | HighAlch: \c07{1:N0}\c | LowAlch: \c07{2:N0}\c".FormatWith(
+                   itemList.Substring(0, itemList.Length - 3), totalHigh, totalLow));
     }
 
   } //class Command
