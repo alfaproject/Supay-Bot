d3c388174040243f2cefcd0967a12c636a9ebacf
alfaproject <alfa.project@gmail.com>
2010-03-12 17:22:02 +0000

    Added support for a custom channel name in WarRemove command.

--- Commands/War/Remove.cs
+++ Commands/War/Remove.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SQLite;
+using System.Text.RegularExpressions;
 
 namespace Supay.Bot {
   static partial class Command {
@@ -9,18 +10,26 @@ namespace Supay.Bot {
         return;
       }
 
-      if (bc.MessageTokens.Length <= 1) {
-        bc.SendReply("Syntax: !WarRemove <player name>");
+      if (bc.MessageTokens.Length < 2) {
+        bc.SendReply("\bSyntax:\b !WarRemove <player name> [#channel]");
         return;
       }
 
-      string rsn = bc.MessageTokens.Join(1).ValidatePlayerName();
+      // get channel name
+      string channelName = bc.Channel;
+      Match matchChannel = Regex.Match(bc.Message, @"#(\S+)");
+      if (matchChannel.Success) {
+        channelName = matchChannel.Groups[1].Value;
+        bc.Message = bc.Message.Replace(matchChannel.Value, string.Empty);
+      }
 
-      if (Database.Lookup<string>("rsn", "warPlayers", "channel=@chan AND rsn=@rsn", new[] { new SQLiteParameter("@chan", bc.Channel), new SQLiteParameter("@rsn", rsn) }) != null) {
-        Database.ExecuteNonQuery("DELETE FROM warPlayers WHERE channel='" + bc.Channel + "' AND rsn='" + rsn + "'");
-        bc.SendReply(@"\b{0}\b was removed from current war.".FormatWith(rsn));
+      string playerName = bc.MessageTokens.Join(1).ValidatePlayerName();
+
+      if (Database.Lookup<string>("rsn", "warPlayers", "channel=@channel AND rsn=@rsn", new[] { new SQLiteParameter("@channel", channelName), new SQLiteParameter("@rsn", playerName) }) != null) {
+        Database.ExecuteNonQuery("DELETE FROM warPlayers WHERE channel='" + channelName + "' AND rsn='" + playerName + "'");
+        bc.SendReply(@"\b{0}\b was removed from current war.".FormatWith(playerName));
       } else {
-        bc.SendReply(@"\b{0}\b isn't signed to current war.".FormatWith(rsn));
+        bc.SendReply(@"\b{0}\b isn't signed to current war.".FormatWith(playerName));
       }
     }
 
