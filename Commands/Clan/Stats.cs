a3506009efce0dc397f502208328ff7007411179
PGertrude <pt-russell@hotmail.com>
2011-04-13 15:32:45 +0100

    Event @all option added

--- Commands/Clan/Stats.cs
+++ Commands/Clan/Stats.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace Supay.Bot {
   static partial class Command {
@@ -20,19 +21,60 @@ namespace Supay.Bot {
         }
       }
 
-      bc.SendReply(@"\bSupreme Skillers\b | Homepage: \c12www.supremeskillers.co.nr\c | \u{0}\u average level: \c07{1}\c (\c07{2:N0}\c average exp.) | Members (\c07{3}\c): \c12http://runehead.com/clans/ml.php?clan=supreme\c".FormatWith(skill, totallevel / ssplayers.Count, totalexp / ssplayers.Count, ssplayers.Count));
+      bc.SendReply(@"\bSupreme Skillers\b | Homepage: \c12http://supremeskillers.net\c | \u{0}\u average level: \c07{1}\c (\c07{2:N0}\c average exp.) | Members (\c07{3}\c): \c12http://services.runescape.com/m=clan-hiscores/members.ws?clanId=314\c".FormatWith(skill, totallevel / ssplayers.Count, totalexp / ssplayers.Count, ssplayers.Count));
     }
 
     public static void Event(CommandContext bc) {
       try {
-        string desc, url;
-        DateTime startTime;
-        string eventPage = new System.Net.WebClient().DownloadString("http://ss.rsportugal.org/api/?module=events&action=getNext");
+        bool all = false;
+        if (bc.Message.Contains("@all")) all = true;
+        DateTime startTime = DateTime.UtcNow;
+        string eventPage = new System.Net.WebClient().DownloadString("http://supremeskillers.net/api/?module=events&action=getNext");
         JObject nextEvent = JObject.Parse(eventPage);
-        startTime = DateTime.ParseExact((string)nextEvent["data"]["startTime"], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-        desc = (string)nextEvent["data"]["description"];
-        url = (string)nextEvent["data"]["url"];
-        bc.SendReply("Next event: \\c07{0}\\c starts in \\c07{1}\\c for more information: \\c12{2}\\c".FormatWith(desc, (startTime - DateTime.UtcNow).ToLongString(), url));
+
+        if (nextEvent["data"] == null) {
+          bc.SendReply("No events currently set for Supreme Skillers. \\c12http://supremeskillers.net/forum/public-events/");
+          return;
+        }
+
+        string[] events = new string[10];
+        int i = -1;
+        foreach (JObject eventData in nextEvent["data"]) {
+          events[++i] = eventData.ToString();
+        }
+
+        if (!all) {
+          nextEvent = JObject.Parse(events[0]);
+          startTime = DateTime.ParseExact((string)nextEvent["startTime"], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+          bc.SendReply("Next event: \\c07{0}\\c starts in \\c07{1}\\c for more information: \\c12{2}\\c"
+            .FormatWith(
+              (string)nextEvent["description"],
+              (startTime - DateTime.UtcNow).ToLongString(),
+              (string)nextEvent["url"])
+            );
+
+        } else {
+          string reply = "Upcoming SS Events: ";
+          for (i = 0; i < 10; i++) {
+            if (events[i] == null) break;
+            nextEvent = JObject.Parse(events[i]);
+            startTime = DateTime.ParseExact((string)nextEvent["startTime"], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            if (i == 0)
+              reply += "{0} (\\c07{1}\\c); "
+                .FormatWith(
+                  (string)nextEvent["description"],
+                  (startTime - DateTime.UtcNow).ToLongString()
+                );
+            else
+              reply += "{0} (\\c07{1}\\c); "
+                .FormatWith(
+                  (string)nextEvent["description"],
+                  String.Format("{0: dddd d MMMM}", startTime, CultureInfo.InvariantCulture).Trim()
+                );
+          }
+          bc.SendReply(reply.Trim());
+  
+        }
       } catch {
         bc.SendReply("Error retrieving next event.");
         return;
