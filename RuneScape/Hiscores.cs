dee09fede915c0dcf37ea2744dafcc83af69f6a9
_aLfa_ <alfa.project@gmail.com>
2011-11-22 17:30:28 +0000

    Fixed new hiscores parsing.

--- RuneScape/Hiscores.cs
+++ RuneScape/Hiscores.cs
@@ -11,19 +11,19 @@ namespace Supay.Bot
       : base(22)
     {
       var wc = new WebClient();
-      string hiscore_page;
 
       if (activity == null)
       {
         this.Name = skill;
 
         // get this skill hiscore table
-        hiscore_page = wc.DownloadString("http://hiscore.runescape.com/overall.ws?table=" + Skill.NameToId(skill) + "&rank=" + rank);
-        foreach (Match M in Regex.Matches(hiscore_page, @"<span class=""rankColumn"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""nameColumn"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""\w+Column"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""xpColumn"">\s+<span>([^<]+)</span>", RegexOptions.Singleline))
+        var hiscorePage = wc.DownloadString("http://hiscore.runescape.com/overall.ws?table=" + Skill.NameToId(skill) + "&rank=" + rank);
+        const string rankRegex = @"<span class=""columnRank"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""columnName"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""columnLevel"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""columnXp"">\s+<span>([^<]+)</span>";
+        foreach (Match m in Regex.Matches(hiscorePage, rankRegex, RegexOptions.Singleline))
         {
-          this.Add(new Skill(this.Name, int.Parse(M.Groups[1].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), int.Parse(M.Groups[3].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), long.Parse(M.Groups[4].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture)));
+          this.Add(new Skill(this.Name, int.Parse(m.Groups[1].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), int.Parse(m.Groups[3].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), long.Parse(m.Groups[4].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture)));
           var newReg = new Regex(@"\W");
-          this[this.Count - 1].RSN = newReg.Replace(M.Groups[2].Value, "_");
+          this[this.Count - 1].RSN = newReg.Replace(m.Groups[2].Value, "_");
         }
       }
       else
@@ -31,12 +31,12 @@ namespace Supay.Bot
         this.Name = activity;
 
         // get this activity hiscore table
-        hiscore_page = wc.DownloadString("http://hiscore.runescape.com/overall.ws?category_type=1&table=" + Activity.NameToId(activity) + "&rank=" + rank);
-
-        foreach (Match M in Regex.Matches(hiscore_page, @"<span class=""rankColumn"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""nameColumn"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""\w+Column"">\s+<span>([^<]+)</span>", RegexOptions.Singleline))
+        var hiscorePage = wc.DownloadString("http://hiscore.runescape.com/overall.ws?category_type=1&table=" + Activity.NameToId(activity) + "&rank=" + rank);
+        const string rankRegex = @"<span class=""columnRank"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""columnName"">\s+<span>([^<]+)</span>\s+</span>\s+<span class=""columnScore"">\s+<span>([^<]+)</span>";
+        foreach (Match m in Regex.Matches(hiscorePage, rankRegex, RegexOptions.Singleline))
         {
-          this.Add(new Activity(this.Name, int.Parse(M.Groups[1].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), int.Parse(M.Groups[3].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture)));
-          this[this.Count - 1].RSN = M.Groups[2].Value.Replace(' ', '_');
+          this.Add(new Activity(this.Name, int.Parse(m.Groups[1].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture), int.Parse(m.Groups[3].Value.Replace(",", string.Empty), CultureInfo.InvariantCulture)));
+          this[this.Count - 1].RSN = m.Groups[2].Value.Replace(' ', '_');
         }
       }
     }
