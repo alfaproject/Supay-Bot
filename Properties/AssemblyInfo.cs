c02c7984de52d2a115ac94de4042c6722c066dba
João Dias <alfa.project@gmail.com>
2012-09-17 21:55:15 +0100

    Changed default framework to 4.5 and channel to #supay

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Mark this assembly with CLSCompliant because it exposes externally visible types.
