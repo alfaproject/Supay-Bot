eacf4248597420e56dbad783833186443a927206
João Dias <alfa.project@gmail.com>
2012-09-18 12:37:59 +0100

    Merged player name getter with player constructor

--- Commands/RuneScape/Stats.cs
+++ Commands/RuneScape/Stats.cs
@@ -103,33 +103,23 @@ namespace Supay.Bot
                 bc.Message = bc.Message.Replace(greaterThanMatch.Value, string.Empty);
             }
 
-            // get rsn
-            string rsn;
-            if (bc.MessageTokens.Length > 1)
+            // get player
+            var player = new Player(bc.GetPlayerName(bc.MessageTokens.Length == 0 ? bc.From.Nickname : bc.MessageTokens.Join(1)));
+            if (!player.Ranked)
             {
-                rsn = bc.GetPlayerName(bc.MessageTokens.Join(1));
-            }
-            else
-            {
-                rsn = bc.GetPlayerName(bc.From.Nickname);
-            }
-
-            var p = new Player(rsn);
-            if (!p.Ranked)
-            {
-                bc.SendReply("\\b{0}\\b doesn't feature Hiscores.".FormatWith(rsn));
+                bc.SendReply("\\b{0}\\b doesn't feature Hiscores.".FormatWith(player.Name));
                 return;
             }
 
             if (ExpNext)
             {
                 string reply = string.Empty;
-                List<Skill> skills = p.Skills.SortedByExpToNextVLevel;
+                List<Skill> skills = player.Skills.SortedByExpToNextVLevel;
                 for (int i = 0; i < skills.Count; i++)
                 {
                     if (i == 0 || i == 15)
                     {
-                        reply = "Exp. to next level of \\b{0}\\b:".FormatWith(rsn);
+                        reply = "Exp. to next level of \\b{0}\\b:".FormatWith(player.Name);
                     }
                     reply += " \\c{0}{1:N0}\\c {2};".FormatWith((skills[i].VLevel > 98 ? "04" : "03"), skills[i].ExpToVLevel, skills[i].Name);
                     if (i == 14 || i == skills.Count - 1)
@@ -144,7 +134,7 @@ namespace Supay.Bot
                 long totalExp = 0,
                      maxTotalExp = 0,
                      maxTotalLevel = 0;
-                foreach (Skill s in p.Skills.Values.Where(s => s.Name != Skill.OVER && s.Name != Skill.COMB))
+                foreach (Skill s in player.Skills.Values.Where(s => s.Name != Skill.OVER && s.Name != Skill.COMB))
                 {
                     long maxSkillExp = s.MaxLevel.ToExp();
                     totalExp += Math.Min(maxSkillExp, s.Exp);
@@ -153,31 +143,31 @@ namespace Supay.Bot
                 }
 
                 // calculate total level and average level
-                int totalLevel = p.Skills[0].Level;
+                int totalLevel = player.Skills[0].Level;
                 if (VLevel)
                 {
                     totalLevel = 0;
-                    for (int i = 1; i < p.Skills.Count - 1; i++)
+                    for (int i = 1; i < player.Skills.Count - 1; i++)
                     {
-                        totalLevel += p.Skills[i].VLevel;
+                        totalLevel += player.Skills[i].VLevel;
                     }
                 }
-                double AvgSkilldouble = Math.Round(totalLevel / (double) (p.Skills.Count - 2), 1);
+                double AvgSkilldouble = Math.Round(totalLevel / (double) (player.Skills.Count - 2), 1);
                 if (Exp)
                 {
-                    AvgSkilldouble = ((long) (p.Skills[0].Exp / (double) (p.Skills.Count - 2))).ToLevel();
+                    AvgSkilldouble = ((long) (player.Skills[0].Exp / (double) (player.Skills.Count - 2))).ToLevel();
                 }
 
-                string reply = "\\b{0}\\b \\c07{3:n}\\c | level: \\c07{1:N0}\\c (\\c07{2}\\c avg.) | exp: \\c07{3:e}\\c (\\c07{4}%\\c of {5}) | rank: \\c07{3:R}\\c".FormatWith(rsn, totalLevel, AvgSkilldouble, p.Skills[0], Math.Round((double) totalExp / maxTotalExp * 100.0, 1), maxTotalLevel);
+                string reply = "\\b{0}\\b \\c07{3:n}\\c | level: \\c07{1:N0}\\c (\\c07{2}\\c avg.) | exp: \\c07{3:e}\\c (\\c07{4}%\\c of {5}) | rank: \\c07{3:R}\\c".FormatWith(player.Name, totalLevel, AvgSkilldouble, player.Skills[0], Math.Round((double) totalExp / maxTotalExp * 100.0, 1), maxTotalLevel);
 
                 var AvgSkill = (int) AvgSkilldouble;
 
                 // add up SS rank if applicable
                 var ssplayers = new Players("SS");
-                if (ssplayers.Contains(p.Name))
+                if (ssplayers.Contains(player.Name))
                 {
                     ssplayers.SortBySkill(Skill.OVER, false);
-                    reply += " (SS rank: \\c07{0}\\c)".FormatWith(ssplayers.IndexOf(rsn) + 1);
+                    reply += " (SS rank: \\c07{0}\\c)".FormatWith(ssplayers.IndexOf(player.Name) + 1);
                 }
 
                 bc.SendReply(reply);
@@ -202,9 +192,9 @@ namespace Supay.Bot
 
                 string replyCombat = "\\uCombat skills\\u:";
                 string replyOther = "\\uOther skills\\u:";
-                for (int i = 1; i < p.Skills.Count - 1; i++)
+                for (int i = 1; i < player.Skills.Count - 1; i++)
                 {
-                    Skill s = p.Skills[i];
+                    Skill s = player.Skills[i];
 
                     if (lessThan > 0 && s.Exp >= lessThan)
                     {
@@ -219,7 +209,7 @@ namespace Supay.Bot
                         continue;
                     }
 
-                    reply = format.FormatWith(s, (VLevel ? s.VLevel : s.Level) > AvgSkill + 7 ? 3 : ((VLevel ? s.VLevel : s.Level) < AvgSkill - 7 ? 4 : 7), s.Exp == p.Skills.Highest[0].Exp ? "\\u" : string.Empty);
+                    reply = format.FormatWith(s, (VLevel ? s.VLevel : s.Level) > AvgSkill + 7 ? 3 : ((VLevel ? s.VLevel : s.Level) < AvgSkill - 7 ? 4 : 7), s.Exp == player.Skills.Highest[0].Exp ? "\\u" : string.Empty);
 
                     if (s.Name != Skill.ATTA && s.Name != Skill.STRE && s.Name != Skill.DEFE && s.Name != Skill.HITP && s.Name != Skill.PRAY && s.Name != Skill.SUMM && s.Name != Skill.RANG && s.Name != Skill.MAGI)
                     {
@@ -230,12 +220,12 @@ namespace Supay.Bot
                         replyCombat += reply;
                     }
                 }
-                bc.SendReply((replyCombat + format.Substring(0, format.Length - 1) + " (\\c07{3}\\c)").FormatWith(p.Skills[Skill.COMB], 7, string.Empty, p.CombatClass));
+                bc.SendReply((replyCombat + format.Substring(0, format.Length - 1) + " (\\c07{3}\\c)").FormatWith(player.Skills[Skill.COMB], 7, string.Empty, player.CombatClass));
                 bc.SendReply(replyOther);
 
                 bool ranked = false;
                 reply = "\\uActivities\\u:";
-                foreach (Activity m in p.Activities.Values)
+                foreach (Activity m in player.Activities.Values)
                 {
                     if (m.Rank > 0)
                     {
