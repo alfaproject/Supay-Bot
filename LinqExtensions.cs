39432dce87dad8616ffd0b7ac5d13cadec6cd158
João Dias <alfa.project@gmail.com>
2012-09-19 13:49:22 +0100

    Added a Linq Extension to Concat a single element

---
+++ LinqExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supay.Bot
+{
+    internal static class LinqExtensions
+    {
+        /// <summary>
+        /// Concatenates an element into a sequence.
+        /// </summary>
+        /// <remarks>
+        /// This method is implemented by using deferred execution.
+        /// The immediate return value is an object that stores all the information that is required to perform the action.
+        /// The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach in Visual C# or For Each in Visual Basic.
+        /// </remarks>
+        /// <typeparam name="TSource">The type of the elements of the sequence.</typeparam>
+        /// <param name="sequence">The sequence to concatenate.</param>
+        /// <param name="element">The element to concatenate to the sequence.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> that contains the concatenated elements of the sequence and element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="sequence"/> is null.</exception>
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> sequence, TSource element)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentNullException("sequence");
+            }
+
+            return sequence.Concat(Enumerable.Repeat(element, 1));
+        }
+    }
+}
